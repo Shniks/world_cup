e1c4e810be81c1a900d18124733a65db27bf7789
Nikhil Shahi <shniks@gmail.com>
2022-07-26 22:29:01 -0700

    Created test to check for player attributes.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe Player do
     expect(player).to be_a Player
   end
 
+  it 'has attributes' do
+    player = Player.new({name: "Luka Modric", position: "midfielder"})
+
+    expect(player.name).to eq("Luka Modric")
+    expect(player.position).to eq("midfielder")
+  end
+
 end
