947c28b389ea82bf232201e0756090f12a8eaef1
Nikhil Shahi <shniks@gmail.com>
2022-07-27 07:48:01 -0700

    Created test to add players.

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -4,22 +4,32 @@ require './spec/spec_helper'
 require './lib/team'
 
 RSpec.describe Team do
-  it 'should be an instance of an object' do
-    team = Team.new("France")
+  before do
+    @team = Team.new("France")
+  end
 
-    expect(team).to be_a Team
+  it 'should be an instance of an object' do
+    expect(@team).to be_a Team
   end
 
   it 'should have attributes' do
-    team = Team.new("France")
+    expect(@team.country).to eq("France")
+    expect(@team.eliminated?).to eq(false)
 
-    expect(team.country).to eq("France")
-    expect(team.eliminated?).to eq(false)
+    @team.eliminated = true
 
-    team.eliminated = true
+    expect(@team.eliminated?).to eq(true)
+    expect(@team.players).to eq([])
+  end
 
-    expect(team.eliminated?).to eq(true)
-    expect(team.players).to eq([])
+  it 'can add players' do
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+    @team.add_player(mbappe)
+    @team.add_player(pogba)
+
+    expect(@team.players).to eq([mbappe, pogba])  
   end
 
 
